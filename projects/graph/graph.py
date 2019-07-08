99b6d831b47dbf4a78e3b8e92f597772f9288e4a
Frulwinn <frulwinn@gmail.com>
2019-07-08 14:21:25 -0500

    things we covered in class

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,16 +7,24 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+        
     def add_vertex(self, vertex):
+        self.vertices[vertext] = set()
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        #pass  # TODO
+
     def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices
+            self.vertices[v1].add(v2)
+        
+        else:
+            raise IndexError("Hey, that vertex doesn't exist!")
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        #pass  # TODO
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
