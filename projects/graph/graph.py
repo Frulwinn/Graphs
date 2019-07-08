eb73c16a0faae91ec70d05bb51d3649448008dfb
Frulwinn <frulwinn@gmail.com>
2019-07-08 14:48:49 -0500

    bft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,7 +7,7 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-        
+
     def add_vertex(self, vertex):
         self.vertices[vertext] = set()
         """
@@ -26,6 +26,27 @@ class Graph:
         """
         #pass  # TODO
     def bft(self, starting_vertex):
+        #keep track of all visited nodes
+        explored = []
+
+        #keep track of nodes to be checked
+        queue = [starting_vertex]
+
+        #keep looping until there are nodes still to be checked
+        while queue:
+            #pop first node from queue
+            node = queue.pop(0)
+            if node not in explored:
+                #add node to list of checked nodes
+                explored.append(node)
+                neighbours = graph[node]
+
+                #add neighbourgs of node to queue
+                for neighbour in neighbours:
+                    queue.append(neighbour)
+                    
+        return explored
+
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
