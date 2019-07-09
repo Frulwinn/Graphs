d06ac1708caa7dd236e304166555a5e886e5b065
Frulwinn <frulwinn@gmail.com>
2019-07-09 17:10:29 -0500

    initial ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,11 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    #create a stack
+    stack = [starting_node]
+    #create a path
+    path = []
+    #create a visited set so we're not caught in infinite loop
+    visited = set()
+    
+    #create a while loop to go through stack till earliest ancestor is reached
+    while stack:
