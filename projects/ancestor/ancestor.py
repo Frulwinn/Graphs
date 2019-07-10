d09852832d08598ca19c4dc7b3c572c6c5cfed16
Frulwinn <frulwinn@gmail.com>
2019-07-10 15:58:00 -0500

    added code for bfs from class and than switched it to do dfs

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,11 +1,101 @@
 
+class Stack:
+	def __init__(self):
+		self.stack = []
+	def push(self, value):
+		self.stack.append(value)
+	def size(self):
+		return len(self.stack)
+	def pop(self):
+		if self.size() > 0:
+			return self.stack.pop(-1)
+		else:
+			return 0
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
+
 def earliest_ancestor(ancestors, starting_node):
-    #create a stack
-    stack = [starting_node]
-    #create a path
-    path = []
-    #create a visited set so we're not caught in infinite loop
-    visited = set()
-    
-    #create a while loop to go through stack till earliest ancestor is reached
-    while stack:
+    # Build the graph
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # Build edges in reverse
+        graph.add_edge(pair[1], pair[0])
+    # Do a BFS (storing the path)
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        # If the path is longer or equal and the value is smaller, or if the path is longer)
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    return earliest_ancestor
+
+
+def earliest_ancestor2(ancestors, starting_node):
+    # Build the graph
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # Build edges in reverse
+        graph.add_edge(pair[1], pair[0])
+    # Do a BFS (storing the path)
+    s = Stack()
+    s.push([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+
+    while s.size() > 0:
+        path = s.pop()
+        v = path[-1]
+        # If the path is longer or equal and the value is smaller, or if the path is longer)
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            s.push(path_copy)
+    return earliest_ancestor
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+print(earliest_ancestor2(test_ancestors, 6))
+#print(earliest_ancestor2)
